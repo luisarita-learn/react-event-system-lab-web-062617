e1f05c59cbd2cbdf0a829f837c29a7ef088c802c
luisarita <github@luisarita.com>
2017-08-29 15:07:29 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Tickler extends React.Component {
+    
+    focus = () => {
+        console.log('Good!');
+    }
+
+    blur = () => {
+        console.log('Hey! Eyes on me!');
+    }
+    
+    render() {
+        return (
+            <button onFocus={this.focus} onBlur={this.blur}>Button Text</button>
+        );
+    }
+}
